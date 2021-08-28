6d474b180462e264ddf85c529a20582012178de7
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2021-08-27 20:07:41 -0400

    index and server files built up, server is running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+const ProjectRouter = require('./project/router.js');
+const ResourceRouter = require('./resource/router.js');
+const TaskRouter = require('./task/router.js');
+
+const server = express();
+
+server.use(express.json());
+
+
+server.use('*', (req, res) =>{
+    res.json({api: 'Test...'})
+})
+
+module.exports = server
