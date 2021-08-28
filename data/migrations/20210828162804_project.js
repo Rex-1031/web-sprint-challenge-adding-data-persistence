26003dbba886ae3352ec07023401148ce22e3142
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2021-08-28 13:13:01 -0400

    resource table created

--- data/migrations/20210828162804_project.js
+++ data/migrations/20210828162804_project.js
@@ -1,16 +1,23 @@
 
 exports.up = async function(knex) {
     //tables with no foreign keys first
-    await knex.schema.createTable('projects', tbl =>{
+    await knex.schema
+    .createTable('project', tbl =>{
         tbl.increments('project_id')
         tbl.string('project_name').notNullable().defaultTo(0)
         tbl.string('project_description')
         tbl.integer('project_completed')
+    })
+    .createTable('resouces', tbl=>{
+        tbl.increments('resource_id')
+        tbl.string('resource_name').notNullable().unique()
+        tbl.string('resource_description')
     })
 };
 
 exports.down =  async function(knex) {
     //tables with no foreign keys last 
     await knex.schema
+    .dropTableIfExists('resources')
     .dropTableIfExists('project')
 };
