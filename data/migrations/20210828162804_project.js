95232c1aa4dc8483731042ad55cec8eb7f54789f
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2021-08-28 13:47:01 -0400

    project_resources table created

--- data/migrations/20210828162804_project.js
+++ data/migrations/20210828162804_project.js
@@ -4,20 +4,50 @@ exports.up = async function(knex) {
     await knex.schema
     .createTable('project', tbl =>{
         tbl.increments('project_id')
-        tbl.string('project_name').notNullable().defaultTo(0)
-        tbl.string('project_description')
-        tbl.integer('project_completed')
+        tbl.string('project_name', 100).notNullable()
+        tbl.string('project_description', 250)
+        tbl.integer('project_completed').unsigned().defaultTo(0)
     })
-    .createTable('resouces', tbl=>{
+    .createTable('resource', tbl=>{
         tbl.increments('resource_id')
-        tbl.string('resource_name').notNullable().unique()
-        tbl.string('resource_description')
+        tbl.string('resource_name', 100).notNullable().unique()
+        tbl.string('resource_description', 250)
     })
+    .createTable('task', tbl=>{
+        tbl.increments('task_id')
+        tbl.string('task_description', 250).notNullable()
+        tbl.string('notes', 250)
+        tbl.integer('task_completed')
+            .unsigned()
+            .notNullable()
+            .defaultTo(0)
+        tbl.integer('project_id')
+            .notNullable()
+            .unsigned()
+            .references('project_id')
+            .inTable('project')
+
+    })
+    .createTable('project_resources', tbl =>{
+        tbl.increments('project_resources_id')
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('project')
+        tbl.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id')
+            .inTable('resource')
+    })
+
 };
 
 exports.down =  async function(knex) {
     //tables with no foreign keys last 
     await knex.schema
+    .dropTableIfExists('task')
     .dropTableIfExists('resources')
     .dropTableIfExists('project')
 };
